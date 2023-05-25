5a6d9295e30f04d6795d5e2caa44b104f7fef246
Oleksii <2veshipropisnieistini@gmail.com>
2023-05-25 12:28:48 +0300

    update

---
+++ index.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-plusplus
+for (let i = 1; i <= 5; i++) {
+  // eslint-disable-next-line no-plusplus
+  for (let j = 1; j <= 5; j++) {
+    const result = i * j;
+    console.log(`${i} x ${j} = ${result}`);
+  }
+  if (i !== 5) {
+    console.log("=>");
+  }
+}
